9254b897ec0d5a1fdf82ebd8e0d894e3f2f9c764
Danylo Bilokha <danbilokha@gmail.com>
2024-07-14 15:24:59 +0200

    chore: style (tailwindcss), ui components (primeng), testing, code styles (eslint, prettier) (#1)
    * chore: tailwind, primeng
    
    * chore: checked testing
    
    * chore: added lint & prettier
    
    * chore: add github build, lint, test action

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { throttleTime } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  // asserting the two objects are equal - required
+  // for TestScheduler assertions to work via your test framework
+  // e.g. using chai.
+  expect(actual).toEqual(expected);
+});
+
+// This test runs synchronously.
+it('generates the stream correctly', () => {
+  testScheduler.run(helpers => {
+    const { cold, time, expectObservable, expectSubscriptions } = helpers;
+    const e1 = cold(' -a--b--c---|');
+    const e1subs = '  ^----------!';
+    const t = time('   ---|       '); // t = 3
+    const expected = '-a-----c---|';
+
+    expectObservable(e1.pipe(throttleTime(t))).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+  });
+});
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,13 +41,16 @@ describe('AppComponent', () => {
   it(`should have the 'machine-test' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('machine-test');
+    expect(app.title).toEqual('Machine Test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, machine-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, it is Machine Test project'
+    );
   });
 });
