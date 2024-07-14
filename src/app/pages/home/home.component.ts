685abe7d0c84ee238f41f8273515a9d13c8d7290
Bilokha Danylo <danbilokha@gmail.com>
2024-07-14 16:57:07 +0200

    feat: get first studies

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { ClinicalTrialServices } from '../../services/clinical-trials.api.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    AsyncPipe,
+    //
     TableModule,
     //
   ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  studies$ = inject(ClinicalTrialServices).getStudies();
+
   products = [
     {
       id: '1000',
--- src/app/services/clinical-trials.api.service.ts
+++ src/app/services/clinical-trials.api.service.ts
@@ -1,6 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, InjectionToken } from '@angular/core';
+import { map } from 'rxjs';
+
+export const ClinicalTrialsAPI = new InjectionToken('ClinicalTrialsAPI');
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClinicalTrialServices {}
+export class ClinicalTrialServices {
+  #http = inject(HttpClient);
+  #path = inject(ClinicalTrialsAPI);
+
+  getStudies() {
+    return (
+      this.#http
+        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+        .get<any>(`${this.#path}/studies`)
+        .pipe(map(({ hits }) => hits))
+    );
+  }
+}
